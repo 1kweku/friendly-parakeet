75e9377bf4ccfa144e7210fb24947271a418953e
1kweku <kwekuplusp@gmail.com>
2022-09-06 12:21:09 -0400

    User prompt added.

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,20 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// generatePassword function defined
+function generatePassword() {
+  var userInput = window.prompt ("Please pick a password length between 8 and 128 characters")
+
+  if (userInput < 8 || userInput > 128) {
+    window.alert("Password length must be between 8 and 128.")
+    return
+  }
+
+  var userNumbers = window.confirm('Do you want numbers in your password?')
+
+}
+
+if 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
